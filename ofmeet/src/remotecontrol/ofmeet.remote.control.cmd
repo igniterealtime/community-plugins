e8001a0844425ff135e81c7bd9a6e9ab7104fe22
deleolajide <dele@uk2.net>
2016-02-16 18:46:13 +0000

    packaged win32 and win64 binaries for remotecontrol

--- ofmeet/src/remotecontrol/ofmeet.remote.control.cmd
+++
@@ -1,9 +0,0 @@
-@echo on
-:: Copyright 2014 The Chromium Authors. All rights reserved.
-:: Use of this source code is governed by a BSD-style license that can be
-:: found in the LICENSE file.
-
-time /t > "%~dp0/ofmeet.remote.control.stdout.log"
-time /t > "%~dp0/ofmeet.remote.control.stderr.log"
-
-"%~dp0/win32/node.exe" "%~dp0/ofmeet.remote.control.js" %* >> "%~dp0/ofmeet.remote.control.stdout.log" 2>> "%~dp0/ofmeet.remote.control.stderr.log"
