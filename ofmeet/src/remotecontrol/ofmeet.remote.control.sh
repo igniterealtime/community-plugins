cb1c5b60bc7985b8af3d56583c962789c5176c16
Dele Olajide <dele@traderlynk.com>
2016-03-09 22:27:26 +0000

    ofmeet - version 0.3.11

--- ofmeet/src/remotecontrol/ofmeet.remote.control.sh
+++ ofmeet/src/remotecontrol/ofmeet.remote.control.sh
@@ -3,4 +3,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-./osx/node ofmeet.remote.control.osx.js
+# TODO - make this work for OSX, Linux32 and Linux64 like install_host.sh
+
+#./osx/node ofmeet.remote.control.osx.js
+#./linux32/node ofmeet.remote.control.linux32.js
+#./linux64/node ofmeet.remote.control.linux64.js
