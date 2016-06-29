9c5868d8dc5635ede50ee30d4dc2fd41f28c3911
deleolajide <dele@uk2.net>
2016-06-29 17:37:59 +0100

    removing build script files

--- ofskype/build.cmd
+++
@@ -1,9 +0,0 @@
-call ant jar >build.txt
-
-rd "C:\openfire_4_0_2\plugins\ofskype" /q /s
-del "C:\openfire_4_0_2\plugins\ofskype.jar"
-copy C:\Projects\ignite\community-plugins\openfire_4_0_2\target\openfire\plugins\ofskype.jar "C:\openfire_4_0_2\plugins"
-
-del "C:\openfire_4_0_2\logs\*.*"
-
-pause
--- ofsocial/build.cmd
+++
@@ -1,8 +0,0 @@
-call ant jar >build.txt
-
-rd "C:\Program Files (x86)\Openfire\plugins\ofsocial" /q /s
-del "C:\Program Files (x86)\Openfire\plugins\ofsocial.jar"
-copy C:\Projects\ignite\community-plugins\openfire_4_0_2\target\openfire\plugins\ofsocial.jar "C:\Program Files (x86)\Openfire\plugins"
-
-del "C:\Program Files (x86)\Openfire\logs\*.*"
-pause
