9adcbff494d01c5f16b34258fb0439ff0995cab6
Guus der Kinderen <guus.der.kinderen@gmail.com>
2017-04-25 11:01:06 +0200

    interface_config.js changes for jitsi-meet update.

--- ofmeet/publicweb/src/main/java/org/jivesoftware/openfire/plugin/ofmeet/InterfaceConfigServlet.java
+++ ofmeet/publicweb/src/main/java/org/jivesoftware/openfire/plugin/ofmeet/InterfaceConfigServlet.java
@@ -46,12 +46,12 @@ public class InterfaceConfigServlet extends HttpServlet
 
             final JSONObject config = new JSONObject();
 
-            config.put( "MAIN_TOOLBAR_BUTTONS",                  new JSONArray( JiveGlobals.getListProperty( "org.jitsi.videobridge.ofmeet.main.toolbar.buttons", Arrays.asList( "microphone", "camera", "desktop", "invite", "hangup" ) ) ) );
-            config.put( "TOOLBAR_BUTTONS",                       new JSONArray( JiveGlobals.getListProperty( "org.jitsi.videobridge.ofmeet.toolbar.buttons",      Arrays.asList( "authentication", "microphone", "camera", "desktop", "recording", "security", "invite", "chat", "etherpad", "sharedvideo", "fullscreen", "sip", "dialpad", "settings", "hangup", "filmstrip", "contacts" ) ) ) );
+            config.put( "MAIN_TOOLBAR_BUTTONS",                  new JSONArray( JiveGlobals.getListProperty( "org.jitsi.videobridge.ofmeet.main.toolbar.buttons", Arrays.asList( "microphone", "camera", "desktop", "invite", "fullscreen", "hangup" ) ) ) );
+            config.put( "TOOLBAR_BUTTONS",                       new JSONArray( JiveGlobals.getListProperty( "org.jitsi.videobridge.ofmeet.toolbar.buttons",      Arrays.asList( "microphone", "camera", "desktop", "invite", "fullscreen", "hangup", "profile", "contacts", "chat", "recording", "etherpad", "sharedvideo", "sip", "dialpad", "settings", "raisehand", "filmstrip" ) ) ) );
             config.put( "SETTINGS_SECTIONS",                     new JSONArray( JiveGlobals.getListProperty( "org.jitsi.videobridge.ofmeet.settings.sections",    Arrays.asList( "language", "devices", "moderator" ) ) ) );
 
             config.put( "CANVAS_EXTRA",                          JiveGlobals.getIntProperty(     "org.jitsi.videobridge.ofmeet.canvas.extra",                  104                 ) );
-            config.put( "CANVAS_RADIUS",                         JiveGlobals.getIntProperty(     "org.jitsi.videobridge.ofmeet.canvas.radius",                 7                   ) );
+            config.put( "CANVAS_RADIUS",                         JiveGlobals.getIntProperty(     "org.jitsi.videobridge.ofmeet.canvas.radius",                 0                   ) );
             config.put( "SHADOW_COLOR",                          JiveGlobals.getProperty(        "org.jitsi.videobridge.ofmeet.shadow.color",                  "#ffffff"           ) );
             config.put( "DEFAULT_BACKGROUND",                    JiveGlobals.getProperty(        "org.jitsi.videobridge.ofmeet.default.background",            "#474747"           ) );
             config.put( "INITIAL_TOOLBAR_TIMEOUT",               JiveGlobals.getIntProperty(     "org.jitsi.videobridge.ofmeet.initial.toolbar.timeout",       20000               ) );
@@ -73,13 +73,19 @@ public class InterfaceConfigServlet extends HttpServlet
             config.put( "RANDOM_AVATAR_URL_PREFIX",              JiveGlobals.getProperty(        "org.jitsi.videobridge.ofmeet.random.avatar.url.prefix",      ""                  ) );
             config.put( "RANDOM_AVATAR_URL_SUFFIX",              JiveGlobals.getProperty(        "org.jitsi.videobridge.ofmeet.random.avatar.url.suffix",      ""                  ) );
             config.put( "FILM_STRIP_MAX_HEIGHT",                 JiveGlobals.getIntProperty(     "org.jitsi.videobridge.ofmeet.film.strip.max.height",         120                 ) );
-            config.put( "LOCAL_THUMBNAIL_RATIO_WIDTH",           JiveGlobals.getIntProperty(     "org.jitsi.videobridge.ofmeet.local.thumbnail.ratio.width",   16                  ) );
-            config.put( "LOCAL_THUMBNAIL_RATIO_HEIGHT",          JiveGlobals.getIntProperty(     "org.jitsi.videobridge.ofmeet.local.thumbnail.ratio.height",  9                   ) );
-            config.put( "REMOTE_THUMBNAIL_RATIO_WIDTH",          JiveGlobals.getIntProperty(     "org.jitsi.videobridge.ofmeet.remote.thumbnail.ration.width", 1                   ) );
-            config.put( "REMOTE_THUMBNAIL_RATIO_HEIGHT",         JiveGlobals.getIntProperty(     "org.jitsi.videobridge.ofmeet.remote.thumbnail.ratio.height", 1                   ) );
             config.put( "ENABLE_FEEDBACK_ANIMATION",             JiveGlobals.getBooleanProperty( "org.jitsi.videobridge.ofmeet.enable.feedback_animation",     false               ) );
             config.put( "DISABLE_FOCUS_INDICATOR",               JiveGlobals.getBooleanProperty( "org.jitsi.videobridge.ofmeet.disable.focus.indicator",       false               ) );
             config.put( "ACTIVE_SPEAKER_AVATAR_SIZE",            JiveGlobals.getIntProperty(     "org.jitsi.videobridge.ofmeet.active.speaker.avatarsize",     100                 ) );
+
+            // Jitsi-meet appears to have replaced LOCAL_THUMBNAIL_RATIO_WIDTH and LOCAL_THUMBNAIL_RATIO_HEIGHT with a combined value in LOCAL_THUMBNAIL_RATIO.
+            final int localThumbnailRatioWidth  = JiveGlobals.getIntProperty("org.jitsi.videobridge.ofmeet.local.thumbnail.ratio.width",16 );
+            final int localThumbnailRatioHeight = JiveGlobals.getIntProperty("org.jitsi.videobridge.ofmeet.local.thumbnail.ratio.height",9 );
+            config.put( "LOCAL_THUMBNAIL_RATIO", localThumbnailRatioWidth + "/" + localThumbnailRatioHeight);
+
+            // Same for REMOTE_THUMBNAIL_RATIO, but for some reason, the resulting value is a single digit.
+            final int remoteThumbnailRatioWidth  = JiveGlobals.getIntProperty("org.jitsi.videobridge.ofmeet.remote.thumbnail.ratio.width",1 );
+            final int remoteThumbnailRatioHeight = JiveGlobals.getIntProperty("org.jitsi.videobridge.ofmeet.remote.thumbnail.ratio.height",1 );
+            config.put( "REMOTE_THUMBNAIL_RATIO", remoteThumbnailRatioWidth / remoteThumbnailRatioHeight );
 
             // Add response headers that instruct not to cache this data.
             response.setHeader( "Expires",       "Sat, 6 May 1995 12:00:00 GMT" );
